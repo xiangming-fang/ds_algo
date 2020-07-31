9cecb4c02f5d2fad9c3ad2c102c4c32b155bfe7f
albert.fang <albert.fang@51job.com>
2020-07-31 11:23:08 +0800

    Initial commit

---
+++ src/com/xm/jy/leetcode/doing/LeetCode_54.java
@@ -0,0 +1,18 @@
+package com.xm.jy.leetcode.doing;
+
+import java.util.List;
+
+/**
+ * @author: albert.fang
+ * @date: 2020/7/27 17:41
+ * @description: 120. 三角形最小路径和
+ * @description:
+给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。
+
+相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+ */
+public class LeetCode_54 {
+    public int minimumTotal(List<List<Integer>> triangle,int index) {
+        return 0;
+    }
+}
