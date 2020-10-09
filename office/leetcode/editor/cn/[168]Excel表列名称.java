fbcf815112e89ef66eea4be85b0ce42b28462ae9
albert.fang <albert.fang@51job.com>
2020-10-09 11:27:35 +0800

    leetcode插件使用

---
+++ office/leetcode/editor/cn/[168]Excel表列名称.java
@@ -0,0 +1,42 @@
+//给定一个正整数，返回它在 Excel 表中相对应的列名称。 
+//
+// 例如， 
+//
+//     1 -> A
+//    2 -> B
+//    3 -> C
+//    ...
+//    26 -> Z
+//    27 -> AA
+//    28 -> AB 
+//    ...
+// 
+//
+// 示例 1: 
+//
+// 输入: 1
+//输出: "A"
+// 
+//
+// 示例 2: 
+//
+// 输入: 28
+//输出: "AB"
+// 
+//
+// 示例 3: 
+//
+// 输入: 701
+//输出: "ZY"
+// 
+// Related Topics 数学 
+// 👍 274 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String convertToTitle(int n) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ office/leetcode/editor/cn/[17]电话号码的字母组合.java
@@ -0,0 +1,25 @@
+//给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。 
+//
+// 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。 
+//
+// 
+//
+// 示例: 
+//
+// 输入："23"
+//输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+// 
+//
+// 说明: 
+//尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。 
+// Related Topics 字符串 回溯算法 
+// 👍 947 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<String> letterCombinations(String digits) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
