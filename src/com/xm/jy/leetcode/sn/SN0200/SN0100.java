b75fadce8c8d6a1696ed2e571cee39f6f1d8b835
albert.fang <albert.fang@51job.com>
2021-05-10 18:59:26 +0800

    SN0100：相同的树

--- src/com/xm/jy/leetcode/sn/SN0200/SN0100.java
+++ src/com/xm/jy/leetcode/sn/SN0200/SN0100.java
@@ -8,25 +8,24 @@ import com.xm.jy.leetcode.util.TreeNodeUtil;
  * @date: 2020/7/27 16:05
  * @description: 100. 相同的树
  * @description: 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * @link: https://leetcode-cn.com/problems/same-tree/
  *
  * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
  */
 public class SN0100 {
-    public static boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null){
-            return true;
-        }
-        if (p == null || q == null){
+    public static boolean solution01(TreeNode p, TreeNode q) {
+        if (p == null && q != null){
             return false;
         }
+        if (p != null && q == null){
+            return false;
+        }
+        if (p == null){
+            return true;
+        }
         if (p.val == q.val){
-            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+            return solution01(p.left,q.left) && solution01(p.right,q.right);
         }
         return false;
-    }
-
-
-    public static void main(String[] args) {
-        isSameTree(TreeNodeUtil.generateTreeNode(new Integer[]{1,2}),TreeNodeUtil.generateTreeNode(new Integer[]{1,null,2}));
     }
 }
