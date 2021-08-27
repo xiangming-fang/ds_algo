b0068181942d9b0f27938822e2c9d84e31933682
albert.fang <albert.fang@51job.com>
2021-08-27 14:16:58 +0800

    反转字符串

---
+++ src/indi/xm/jy/leetcode/sn/SN0400/SN0344.java
@@ -0,0 +1,35 @@
+package indi.xm.jy.leetcode.sn.SN0400;
+
+import org.junit.Test;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN0400
+ * @ClassName: SN0344
+ * @Author: albert.fang
+ * @Description: 反转字符串
+ * @Date: 2021/8/27 14:03
+ */
+public class SN0344 {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left <= right){
+            swap(s,left,right);
+            left ++;
+            right --;
+        }
+        System.out.println(s);
+    }
+
+    private void swap(char[] s,int l,int r){
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+    }
+
+    @Test
+    public void test(){
+        reverseString(new char[]{'d','r','o','w'});
+    }
+}
