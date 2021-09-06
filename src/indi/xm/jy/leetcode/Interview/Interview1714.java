9665fdbca31a24241887ebf7dc61e23db3d34852
albert.fang <albert.fang@51job.com>
2021-09-06 18:34:08 +0800

    快排

--- src/indi/xm/jy/leetcode/Interview/Interview1714.java
+++ src/indi/xm/jy/leetcode/Interview/Interview1714.java
@@ -11,7 +11,8 @@ import java.util.PriorityQueue;
  * @Date: 2021/9/3 9:39
  */
 public class Interview1714 {
-    public int[] smallestK(int[] arr, int k) {
+    // 最小堆
+    public int[] smallestK01(int[] arr, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
         for (int s : arr) {
             pq.offer(s);
@@ -21,5 +22,48 @@ public class Interview1714 {
             res[i] = pq.poll();
         }
         return res;
+    }
+
+    // 快排
+    public int[] smallestK(int[] arr, int k) {
+        quickSort(arr,0,arr.length - 1);
+        int[] res = new int[k];
+        System.arraycopy(arr,0,res,0,res.length);
+        return res;
+    }
+
+    private void quickSort(int[] arr,int left,int right){
+        if (left >= right){
+            return;
+        }
+        int start = left;
+        int end = right;
+        int cardinalNum = arr[left];
+        while (left < right){
+            if (arr[left] > cardinalNum && arr[right] < cardinalNum){
+                swap(arr,left,right);
+            }
+            if (arr[left] <= cardinalNum){
+                left ++;
+            }
+            if (arr[right] >= cardinalNum){
+                right --;
+            }
+        }
+        if (arr[left] < arr[start]){
+            swap(arr,start,left);
+            quickSort(arr,start,left - 1);
+            quickSort(arr,left + 1,end);
+        }
+        else {
+            quickSort(arr,start,left - 1);
+            quickSort(arr,left,end);
+        }
+    }
+
+    private void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j]= temp;
     }
 }
