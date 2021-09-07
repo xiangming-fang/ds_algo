8c91dbfd58a71a2ab706f39ee27c4ed945197dda
albert.fang <albert.fang@51job.com>
2021-09-07 17:49:32 +0800

    杨辉三角 II

---
+++ src/indi/xm/jy/leetcode/sn/SN0200/SN0119.java
@@ -0,0 +1,40 @@
+package indi.xm.jy.leetcode.sn.SN0200;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN0200
+ * @ClassName: SN0119
+ * @Author: albert.fang
+ * @Description: 杨辉三角 II
+ * @Date: 2021/9/7 17:31
+ */
+public class SN0119 {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> yh = generate(rowIndex);
+        return yh.get(yh.size() - 1);
+    }
+
+    private List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> one = new ArrayList<>();
+        one.add(1);
+        result.add(one);
+        if (numRows == 1){
+            return result;
+        }
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> pre = result.get(i - 1);
+            List<Integer> list = new ArrayList<>();
+            list.add(pre.get(0));
+            for (int j = 1; j < i ; j++) {
+                list.add(pre.get(j) + pre.get(j - 1));
+            }
+            list.add(pre.get(pre.size() - 1));
+            result.add(list);
+        }
+        return result;
+    }
+}
