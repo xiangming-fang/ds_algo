a2acb32ad9849acc8ed9d9411eed3bee9481aa7f
albert.fang <albert.fang@51job.com>
2021-09-10 18:13:00 +0800

    高频面试题

---
+++ src/indi/xm/jy/leetcode/sn/SN0200/SN0152.java
@@ -0,0 +1,23 @@
+package indi.xm.jy.leetcode.sn.SN0200;
+
+import org.junit.Test;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN0200
+ * @ClassName: SN0152
+ * @Author: albert.fang
+ * @Description: 乘积最大子数组
+ * @Date: 2021/9/7 16:46
+ */
+public class SN0152 {
+    public int maxProduct(int[] nums) {
+        return 0;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(maxProduct(new int[]{2, 3, -2, 4}));
+        System.out.println(maxProduct(new int[]{-2,0,-1,1,2,2,}));
+    }
+}
