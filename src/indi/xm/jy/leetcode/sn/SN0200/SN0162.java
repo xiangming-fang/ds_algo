712083e8409c34c6351acc7ff318596e328dc9bc
albert.fang <albert.fang@51job.com>
2021-09-15 11:38:30 +0800

    寻找峰值

---
+++ src/indi/xm/jy/leetcode/sn/SN0200/SN0162.java
@@ -0,0 +1,22 @@
+package indi.xm.jy.leetcode.sn.SN0200;
+
+import org.junit.Test;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN0200
+ * @ClassName: SN0162
+ * @Author: albert.fang
+ * @Description: 寻找峰值
+ * @Date: 2021/9/15 9:56
+ */
+public class SN0162 {
+    public int findPeakElement(int[] nums) {
+        return Integer.parseInt("不要太着急嘛");
+    }
+
+    @Test
+    public void test(){
+        System.out.println(findPeakElement(null));
+    }
+}
