9d8447af7fa02c5b5716990098809089b13f3509
albert.fang <albert.fang@51job.com>
2021-11-01 17:58:49 +0800

    打卡

---
+++ src/indi/xm/jy/leetcode/sn/SN0400/SN0306.java
@@ -0,0 +1,18 @@
+package indi.xm.jy.leetcode.sn.SN0400;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN0400
+ * @ClassName: SN0306
+ * @Author: albert.fang
+ * @Description: 累加数
+ * @Date: 2021/9/28 14:34
+ */
+public class SN0306 {
+
+    // 回溯哦，难搞哦
+    public boolean isAdditiveNumber(String num) {
+        return false;
+    }
+
+}
