7012cfed718b4b659e8b9f2e854cc4a7f8685d0a
albert.fang <albert.fang@51job.com>
2021-11-19 14:01:50 +0800

    22.括号生成

---
+++ src/indi/xm/jy/leetcode/util/StringUtils.java
@@ -0,0 +1,19 @@
+package indi.xm.jy.leetcode.util;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.util
+ * @ClassName: StringUtils
+ * @Author: albert.fang
+ * @Description:
+ * @Date: 2021/11/16 20:26
+ */
+public class StringUtils {
+
+    public static void replaceChar(String str,char[] oldChar,char[] newChar){
+        for (int i = 0; i < oldChar.length; i++) {
+            str = str.replace(oldChar[i], newChar[i]);
+        }
+        System.out.println(str);
+    }
+}
