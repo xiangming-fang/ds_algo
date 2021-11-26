1e635a31179cda5d0032a01a1e20c937f9f1a941
albert.fang <albert.fang@51job.com>
2021-11-26 14:34:41 +0800

    Offer32_2 bfs

---
+++ src/indi/xm/jy/leetcode/offer/Offer32_2.java
@@ -0,0 +1,42 @@
+package indi.xm.jy.leetcode.offer;
+
+
+import indi.xm.jy.leetcode.data_structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.offer
+ * @ClassName: Offer32_2
+ * @Author: albert.fang
+ * @Description: 从上到下打印二叉树
+ * @Date: 2021/11/26 14:32
+ */
+public class Offer32_2 {
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int len = queue.size();
+            List<Integer> single = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                TreeNode poll = queue.poll();
+                if (poll != null){
+                    single.add(poll.val);
+                    queue.offer(poll.left);
+                    queue.offer(poll.right);
+                }
+            }
+            if (!single.isEmpty()){
+                res.add(single);
+            }
+        }
+        return res;
+    }
+}
