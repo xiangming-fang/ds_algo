921e0c99acf8f48a5c2870373f88bd042309b236
albert.fang <albert.fang@51job.com>
2021-12-22 10:43:54 +0800

    伪KMP算法

---
+++ src/indi/xm/jy/leetcode/sn/SN1000/SN0686.java
@@ -0,0 +1,85 @@
+package indi.xm.jy.leetcode.sn.SN1000;
+
+import indi.xm.jy.leetcode.util.StringUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN1000
+ * @ClassName: SN0686
+ * @Author: albert.fang
+ * @Description: 686. 重复叠加字符串匹配
+ * @Date: 2021/12/22 9:40
+ */
+public class SN0686 {
+
+    public int repeatedStringMatch(String a, String b) {
+        int res = 0;
+        StringBuilder sb = new StringBuilder();
+
+        return -1;
+    }
+
+    // 伪KMP
+    private int isSubString(String p,String s){
+
+        if (s.length() > p.length()){
+            return -1;
+        }
+        char c = s.charAt(0);
+        ArrayList<Integer> firstIndexs = new ArrayList<>();
+        for (int i = 0; i <= p.length() - s.length(); i++) {
+            if (p.charAt(i) == c){
+                firstIndexs.add(i);
+            }
+        }
+
+        for (Integer index : firstIndexs) {
+            int count = 0;
+            int tmpIndex = index;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) != p.charAt(tmpIndex++)){
+                    break;
+                }
+                count ++;
+            }
+            if (count == s.length()){
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public boolean detector(){
+        String str1 = StringUtils.getRandomString(5, 20);
+        String str2 = StringUtils.getRandomString(5, 5);
+        if (isSubString(str1,str2) != str1.indexOf(str2)) {
+            System.out.println(str1);
+            System.out.println(str2);
+            return false;
+        }
+        return true;
+    }
+
+    @Test
+    public void test(){
+
+        for (int i = 0; i < 5000000; i++) {
+            if (!detector()) {
+                System.out.println(i);
+                throw new RuntimeException("伪KMP写错了");
+            }
+        }
+        System.out.println("伪KMP写的完全正确");
+    }
+
+    @Test
+    public void testDet(){
+        System.out.println(isSubString("cebeeebdeecaabb", "eb"));
+    }
+
+}
--- src/indi/xm/jy/leetcode/util/StringUtils.java
+++ src/indi/xm/jy/leetcode/util/StringUtils.java
@@ -1,5 +1,7 @@
 package indi.xm.jy.leetcode.util;
 
+import java.util.Random;
+
 /**
  * @ProjectName: leetcode
  * @Package: indi.xm.jy.leetcode.util
@@ -20,5 +22,20 @@ public class StringUtils {
     public static void replaceArr(String str){
         replaceChar(str,new char[]{'[',']','"','\n'},
                 new char[]{'{','}','\'',' '});
+    }
+
+    /**
+     * 生成随机字符串
+     *
+     * @param possibilities
+     * @param size
+     * @return
+     */
+    public static String getRandomString(int possibilities, int size) {
+        char[] ans = new char[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (char) ((int) (Math.random() * possibilities) + 'a');
+        }
+        return String.valueOf(ans);
     }
 }
