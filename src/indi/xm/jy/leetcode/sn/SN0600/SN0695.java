483ccaff9538d5fb4d5806ac4602632a01150350
albert.fang <albert.fang@51job.com>
2022-01-04 13:49:24 +0800

    两道简单的dfs 695、岛屿最大面积 733、像素渲染

--- src/indi/xm/jy/leetcode/sn/SN0600/SN0695.java
+++ src/indi/xm/jy/leetcode/sn/SN0600/SN0695.java
@@ -11,7 +11,8 @@ import org.junit.Test;
  * @Date: 2021/9/1 16:19
  */
 public class SN0695 {
-    public int maxAreaOfIsland(int[][] grid) {
+
+    public int maxAreaOfIsland01(int[][] grid) {
         int max = 0;
         int row = grid.length;
         int col = grid[0].length;
@@ -41,6 +42,47 @@ public class SN0695 {
         count += dfs(grid,r,c - 1,row,col);
         count += dfs(grid,r,c + 1,row,col);
         return count;
+    }
+
+    /******************* 2022-01-04 **************************/
+
+    private int m,n;
+    private boolean[][] visited;
+    // 上右下左
+    private int[][] direction = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
+    private int res;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        visited = new boolean[m][n];
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (!visited[row][col] && grid[row][col] == 1) {
+                    res = Math.max(dfs(grid,row,col),res);
+                }
+            }
+        }
+        return res;
+    }
+
+    private int dfs(int[][] grid, int row, int col) {
+        if (!isTruePos(row,col)) return 0;
+        if (visited[row][col]) return 0;
+        if (grid[row][col] == 0) return 0;
+        visited[row][col] = true;
+        int ans = 1;
+        for (int[] dir : direction) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            ans += dfs(grid,newRow,newCol);
+        }
+        return ans;
+    }
+
+    // true - 合法位置
+    private boolean isTruePos(int row,int col){
+        return row >= 0 && row < m && col >=0 && col < n;
     }
 
     @Test
--- src/indi/xm/jy/leetcode/sn/SN0600/SN0733.java
+++ src/indi/xm/jy/leetcode/sn/SN0600/SN0733.java
@@ -13,7 +13,8 @@ import java.util.Arrays;
  * @Date: 2021/9/1 15:29
  */
 public class SN0733 {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+
+    public int[][] floodFill01(int[][] image, int sr, int sc, int newColor) {
         int row = image.length;
         int col = image[0].length;
         int oldColor = image[sr][sc];
@@ -50,9 +51,43 @@ public class SN0733 {
         dfs(image,r,c + 1,row,col,oldColor,newColor,visited);
     }
 
+    /******************* 2022-01-04 **************************/
+    private int m,n;
+    private boolean[][] visited;
+    // 上右下左
+    private int[][] direction = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        m = image.length;
+        n = image[0].length;
+        visited = new boolean[m][n];
+        dfs(image,sr,sc,newColor,image[sr][sc]);
+        return image;
+    }
+
+    private void dfs(int[][] image, int row, int col, int newColor,int chgImg) {
+        if (!isTruePos(row,col)) return;
+        if (visited[row][col]) return;
+        image[row][col] = newColor;
+        visited[row][col] = true;
+        for (int[] dir : direction) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            // 合法 & 和之前的位置元素相等
+            if (isTruePos(newRow,newCol) && chgImg == image[newRow][newCol]) dfs(image,newRow,newCol,newColor,chgImg);
+        }
+    }
+
+    // true - 合法位置
+    private boolean isTruePos(int row,int col){
+        return row >= 0 && row < m && col >=0 && col < n;
+    }
+
     @Test
     public void test(){
-        System.out.println(Arrays.deepToString(floodFill(new int[][]{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}, 1, 1, 1)));
-        System.out.println(Arrays.deepToString(floodFill(new int[][]{{0,0,0}, {0,1,1}}, 1, 1, 1)));
+        System.out.println(Arrays.deepToString(floodFill(new int[][]{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}, 1, 1, 2)));
+        System.out.println(Arrays.deepToString(floodFill(new int[][]{{0,0,0}, {0,1,1}}, 1, 1, 2)));
+        System.out.println(Arrays.deepToString(floodFill(new int[][]{{0,0,0}, {0,0,0}}, 0, 0, 2)));
+
     }
 }
