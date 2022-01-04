c2e6ec58b5a2fe4b5c856cbf054caecdb48d4046
albert.fang <albert.fang@51job.com>
2022-01-04 11:50:43 +0800

    位运算： n & (n - 1) 去掉n的最后一个1 n & (~n + 1) 得到n的最后一个1

--- src/indi/xm/jy/leetcode/sn/SN0200/SN0191.java
+++ src/indi/xm/jy/leetcode/sn/SN0200/SN0191.java
@@ -6,10 +6,20 @@ import org.junit.Test;
 // 位1的个数
 public class SN0191 {
     // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
+    public int hammingWeight01(int n) {
         int res = 0;
         while ( n != 0){
             n = n & (n - 1);
+            res ++;
+        }
+        return res;
+    }
+
+    public int hammingWeight(int n) {
+        int num = n > 0 ? n : -n;
+        int res = 0;
+        while (num != 0){
+            num = num & (num - 1);
             res ++;
         }
         return res;
--- src/indi/xm/jy/leetcode/sn/SN0400/SN0231.java
+++ src/indi/xm/jy/leetcode/sn/SN0400/SN0231.java
@@ -4,11 +4,24 @@ import org.junit.Test;
 
 // 2的幂
 public class SN0231 {
-    public boolean isPowerOfTwo(int n) {
+    public boolean isPowerOfTwo01(int n) {
         if (n == 0){
             return false;
         }
         return (n & (n - 1)) == 0;
+    }
+
+
+    // 如果是2的幂次方的话，那么这个n的二进制肯定只有一个1
+    public boolean isPowerOfTwo02(int n) {
+        if (n <= 0) return false;
+        return (n & (n - 1)) == 0;
+    }
+
+    // n & (~n + 1) 提取 n 的最后一个1
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        return (n & (~n + 1)) == n;
     }
 
     @Test
