33a14f5cbd3ab07ce9f251f63bd2ebb5ce75b710
albert.fang <albert.fang@51job.com>
2022-01-04 16:01:52 +0800

    206. 反转链表

--- src/indi/xm/jy/leetcode/sn/SN0400/SN0206.java
+++ src/indi/xm/jy/leetcode/sn/SN0400/SN0206.java
@@ -2,6 +2,8 @@ package indi.xm.jy.leetcode.sn.SN0400;
 
 import indi.xm.jy.leetcode.data_structure.ListNode;
 
+import java.util.Stack;
+
 /**
  * @author: albert.fang
  * @date: 2020/7/21 13:21
@@ -25,14 +27,32 @@ public class SN0206 {
     }
 
     // 递归 —— 直接递归到链表的末尾开始操作
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList02(ListNode head) {
         if (head == null || head.next == null){
             return head;
         }
-        ListNode newHead = reverseList(head.next);
+        ListNode newHead = reverseList02(head.next);
         head.next.next = head;
         head.next = null;
         return newHead;
+    }
+
+    /******************* 2022-01-04 **************************/
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while ( cur != null) {
+            // 1、先保存下面一个
+            ListNode next = cur.next;
+            // 2、改变当前节点下个指针，指向前面一个
+            cur.next = pre;
+            // 3、pre 指针，下移到cur位置
+            pre = cur;
+            // 4、cur 指针后移
+            cur = next;
+        }
+        return pre;
     }
 
 }
--- src/indi/xm/jy/leetcode/sn/SN0600/SN0542.java
+++ src/indi/xm/jy/leetcode/sn/SN0600/SN0542.java
@@ -10,4 +10,37 @@ package indi.xm.jy.leetcode.sn.SN0600;
  */
 public class SN0542 {
 
+    private int m,n;
+    // 上右下左
+    private int[][] direction = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
+
+    public int[][] updateMatrix(int[][] mat) {
+        m = mat.length;
+        n = mat[0].length;
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (mat[row][col] != 0)  dfs(mat,row,col);
+            }
+        }
+        return mat;
+    }
+
+    // 找到row,col这个位置距离0最近的距离是多少
+    private int dfs(int[][] mat, int row, int col) {
+        if (!isTruePos(row,col)) return 0;
+        if (mat[row][col] == 0) return 0;
+        int ret = 1;
+        for (int[] ans : direction) {
+            int newRow = row + ans[0];
+            int newCol = col + ans[1];
+            ret += dfs(mat,newRow,newCol);
+        }
+        return ret;
+    }
+
+    // true - 合法位置
+    private boolean isTruePos(int row,int col){
+        return row >= 0 && row < m && col >=0 && col < n;
+    }
+
 }
