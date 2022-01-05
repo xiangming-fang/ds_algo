80dace871bc6a2a40927561989b15295decdbd47
albert.fang <albert.fang@51job.com>
2022-01-05 09:43:26 +0800

    136.只出现一次的数字

--- src/indi/xm/jy/leetcode/sn/SN0200/SN0136.java
+++ src/indi/xm/jy/leetcode/sn/SN0200/SN0136.java
@@ -6,6 +6,16 @@ import java.util.Arrays;
  * 只出现一次的数字
  */
 public class SN0136 {
+    public int singleNumber01(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+
+    /**********************2022-01-05***********************/
+
     public int singleNumber(int[] nums) {
         int res = 0;
         for (int num : nums) {
@@ -13,4 +23,5 @@ public class SN0136 {
         }
         return res;
     }
+
 }
