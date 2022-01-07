d1314fec54ab594c3b1e2bae734d0403701a895b
albert.fang <albert.fang@51job.com>
2022-01-07 19:14:26 +0800

    打卡

---
+++ src/indi/xm/jy/leetcode/sn/SN2000/SN1614.java
@@ -0,0 +1,27 @@
+package indi.xm.jy.leetcode.sn.SN2000;
+
+import java.util.Stack;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN2000
+ * @ClassName: SN1614
+ * @Author: albert.fang
+ * @Description: 1614. 括号的最大嵌套深度
+ * @Date: 2022/1/7 18:48
+ */
+public class SN1614 {
+    public int maxDepth(String s) {
+        int res = 0;
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (aChar == '(') {
+                stack.push(aChar);
+                res = Math.max(res,stack.size());
+            }
+            else if (aChar == ')') stack.pop();
+        }
+        return res;
+    }
+}
