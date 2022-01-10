f017940bb5f72894d365ca62f2d12200f3bebefb
albert.fang <albert.fang@51job.com>
2022-01-10 18:30:58 +0800

    累加数 回溯 打卡题

--- src/indi/xm/jy/leetcode/sn/SN0400/SN0306.java
+++ src/indi/xm/jy/leetcode/sn/SN0400/SN0306.java
@@ -20,7 +20,7 @@ public class SN0306 {
 
     // 2021 11 01 回溯哦，难搞哦
     // 2021 11 22 我来啦，回溯哦，多练哦
-    public boolean isAdditiveNumber(String num) {
+    public boolean isAdditiveNumber01(String num) {
         return dfs(num);
     }
 
@@ -66,9 +66,88 @@ public class SN0306 {
         return tempStore.get(len - 1) + tempStore.get(len - 2) == pathValue;
     }
 
+    /********************** 2022-01-10 打卡题 ********************/
+
+    private List<String> numList = new ArrayList<>();
+
+    public boolean isAdditiveNumber(String num){
+        return isMatch(num);
+    }
+
+    private boolean isMatch(String num){
+
+        if (numList.size() >= 3 && "".equals(num)){
+            System.out.println(numList);
+            return true;
+        }
+
+        for (int i = 1; i <= num.length(); i++) {
+            String path = num.substring(0, i);
+            if (path.length() > 1 && path.startsWith("0")) return false;
+            if (stepIsTrue(path)){
+                numList.add(path);
+                if (isMatch(num.substring(i))) return true;
+                numList.remove(numList.size() - 1);
+            }
+        }
+
+        return false;
+    }
+
+    // 切割路径上的值
+    private boolean stepIsTrue(String path){
+        int size = numList.size();
+        if (size <= 1){
+            return true;
+        }
+        return isTwoNumAdd(numList.get(size - 1),numList.get(size - 2),path);
+    }
+
+    private boolean isTwoNumAdd(String one,String two,String ret){
+
+        int oneLen = one.length();
+        int twoLen = two.length();
+        if ( oneLen > ret.length() || twoLen > ret.length()) return false;
+
+        char[] firstChars = oneLen >= twoLen ? one.toCharArray() : two.toCharArray();
+        char[] secondChars = oneLen < twoLen ? one.toCharArray() : two.toCharArray();
+
+        int firstLen = Math.max(oneLen, twoLen);
+        int secondLen = Math.min(oneLen, twoLen);
+
+        StringBuilder tempRet = new StringBuilder();
+
+        int preCarry = 0;
+
+        while (--firstLen >= 0){
+
+            int fInt = firstChars[firstLen] - '0';
+            int sInt = 0;
+            if ( --secondLen >= 0 ) {
+                sInt = secondChars[secondLen] - '0';
+            }
+            int num = fInt + sInt + preCarry;
+            String onceRet = String.valueOf(num);
+            tempRet.insert(0,onceRet.charAt(onceRet.length() - 1));
+            if (num >= 10) preCarry = onceRet.charAt(0) - '0';
+            else preCarry = 0;
+        }
+
+        if (preCarry > 0){
+            tempRet.insert(0,preCarry);
+        }
+
+        return tempRet.toString().equals(ret);
+
+    }
+
+
     @Test
     public void test(){
+//        int n = ' ';
+//        System.out.println(n);
 //        System.out.println(isAdditiveNumber("112358"));
+//        System.out.println(isAdditiveNumber("112"));
 //        System.out.println(isAdditiveNumber("199100199"));
 //        System.out.println(isAdditiveNumber("1991000199299498797"));
 //        System.out.println(isAdditiveNumber("11235813213455890144"));
@@ -90,9 +169,13 @@ public class SN0306 {
 //        System.out.println(isAdditiveNumber("2461016264268110179"));
 //        System.out.println(isAdditiveNumber("11235813213455890144"));
 //        System.out.println(isAdditiveNumber("101"));
-        System.out.println(isAdditiveNumber("11111111111111111111111111111111111112111111111111111111111111111111111111122"));
-        System.out.println(tempStore);
+//        System.out.println(isAdditiveNumber("11111111111111111111111111111111111112111111111111111111111111111111111111122"));
+//        System.out.println(tempStore);
 //        System.out.println(Integer.parseInt("03".substring(0, 2)));
+//        System.out.println(isTwoNumAdd("1111111111111111111111111111111111111", "1", "1111111111111111111111111111111111113"));
+//        System.out.println(isTwoNumAdd("1", "9999999999999999999999999999999999999", "10000000000000000000000000000000000000"));
+//        System.out.println(isAdditiveNumber("1999999999999999999999999999999999999910000000000000000000000000000000000000"));
+        System.out.println(isAdditiveNumber("1999999999999999999999999999999999999910000000000000000000000000000000000000"));
     }
 
 }
