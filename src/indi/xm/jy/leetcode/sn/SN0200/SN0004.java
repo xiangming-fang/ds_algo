410d1335cb0f4ef7bc0e009e29de7b3a66342ecd
xiangming.fang <fang.albert@foxmail.com>
2022-09-24 18:55:36 +0800

    4.寻找两个正序数的中位数

---
+++ src/indi/xm/jy/leetcode/sn/SN0200/SN0004.java
@@ -0,0 +1,67 @@
+package indi.xm.jy.leetcode.sn.SN0200;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN0200
+ * @ClassName: SN0004
+ * @Author: albert.fang
+ * @Description: 4. 寻找两个正序数组的中位数
+ * @Date: 2022/5/30 16:01
+ */
+public class SN0004 {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len = nums1.length + nums2.length;
+        if ((len & 1) == 0){
+            return (findKthNum2(nums1,nums2,len/2) + findKthNum2(nums1,nums2,len/2 + 1))/2;
+        }
+        return findKthNum2(nums1,nums2,len/2 + 1);
+    }
+
+    // nums1、nums2是有序数组
+    // 找到数组nums1和nums2中第k个小的数字 并且 k 合法
+    public double findKthNum(int[] nums1,int[] nums2,int k){
+        int[] merge = new int[nums1.length + nums2.length];
+        System.arraycopy(nums1,0,merge,0,nums1.length);
+        System.arraycopy(nums2,0,merge,nums1.length, nums2.length);
+        Arrays.sort(merge);
+        return merge[k - 1];
+    }
+
+    public double findKthNum2(int[] nums1,int[] nums2,int k){
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        int p1 = 0,p2 = 0;
+        while (maxHeap.size() < k){
+            if (indexIsLegal(p1,nums1) && !indexIsLegal(p2,nums2)){
+                maxHeap.offer(nums1[p1++]);
+            }
+            else if (!indexIsLegal(p1,nums1) && indexIsLegal(p2,nums2)){
+                maxHeap.offer(nums2[p2++]);
+            }
+            else {
+                if (nums1[p1] <= nums2[p2]){
+                    maxHeap.offer(nums1[p1++]);
+                }
+                else {
+                    maxHeap.offer(nums2[p2++]);
+                }
+            }
+        }
+        return maxHeap.peek();
+    }
+
+    // 下标是否合法
+    public boolean indexIsLegal(int index,int[] arr){
+        return index >= 0 && index < arr.length;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(findMedianSortedArrays(new int[]{1, 3}, new int[]{2, 3}));
+    }
+}
