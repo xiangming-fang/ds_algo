f445e5523c20d9d7661f5f06f43c7284a1292d28
xiangming.fang <fang.albert@foxmail.com>
2022-10-12 10:17:41 +0800

    打卡

---
+++ src/indi/xm/jy/leetcode/sn/SN1000/SN0817.java
@@ -0,0 +1,31 @@
+package indi.xm.jy.leetcode.sn.SN1000;
+
+import indi.xm.jy.leetcode.data_structure.ListNode;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN1000
+ * @ClassName: SN0817
+ * @Author: albert.fang
+ * @Description: 817. 链表组件
+ * @Date: 2022/10/12 9:49
+ */
+public class SN0817 {
+
+    public int numComponents(ListNode head, int[] nums) {
+        boolean[] hashtable = new boolean[10000];
+        for (int num : nums) {
+            hashtable[num] = true;
+        }
+        int count = 0;
+        ListNode p = head;
+        while (p != null){
+            if (hashtable[p.val] && (p.next == null || !hashtable[p.next.val])) {
+                count ++;
+            }
+            p = p.next;
+        }
+        return count;
+    }
+
+}
