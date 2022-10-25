94fc468c9cb4688cca22a0bf5bd469badaed5626
方翔鸣 <48317891+xiangming-fang@users.noreply.github.com>
2022-10-25 19:15:57 +0800

    acWing - 788 - 逆序对

--- src/indi/xm/jy/acwing/questions/s788/Main.java
+++ src/indi/xm/jy/acwing/questions/s788/Main.java
@@ -14,7 +14,6 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
-    static int res = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -23,6 +22,24 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Integer.parseInt(strs[i]);
         }
-        System.out.print(res);
+        System.out.print(quickSort(arr,0,arr.length - 1));
+    }
+    public static long quickSort(int[] arr, int l, int r){
+        if (l >= r) return 0;
+        int mid = l + (r-l)/2;
+        long res = quickSort(arr,l,mid) + quickSort(arr,mid+1,r);
+        int i = l,j = mid + 1,k = 0;
+        int[] tmp = new int[r - l + 1];
+        while (i<=mid && j <= r){
+            if (arr[i] <= arr[j]) tmp[k++] = arr[i++];
+            else {
+                tmp[k++] = arr[j++];
+                res += mid - i + 1;
+            }
+        }
+        while (i<=mid) tmp[k++] = arr[i++];
+        while (j<=r) tmp[k++] = arr[j++];
+        for (i = l; i <= r; i++) arr[i] = tmp[i-l];
+        return res;
     }
 }
