dc1f88f09afe4b924f8c5dcfe0c72a4bca3309fc
方翔鸣 <48317891+xiangming-fang@users.noreply.github.com>
2022-10-27 16:01:48 +0800

    一维前缀和、二维前缀和：解决区间和问题

---
+++ src/indi/xm/jy/acwing/questions/prefixsum/s0795/Main.java
@@ -0,0 +1,37 @@
+package indi.xm.jy.acwing.questions.prefixsum.s0795;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.acwing.questions.prefixsum.s0795
+ * @ClassName: Main
+ * @Author: albert.fang
+ * @Description: 前缀和
+ * @Date: 2022/10/27 12:21
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String first = br.readLine();
+        int n = Integer.parseInt(first.split(" ")[0]);
+        int m = Integer.parseInt(first.split(" ")[1]);
+        int[] arr = new int[n+1];
+        String[] second = br.readLine().split(" ");
+        for (int i = 1; i <= second.length; i++) {
+            arr[i] = Integer.parseInt(second[i-1]);
+        }
+        int[] sum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            sum[i ] = sum[i-1] + arr[i];
+        }
+        for (int i = 0; i < m; i++) {
+            String[] s = br.readLine().split(" ");
+            int start = Integer.parseInt(s[0]);
+            int end = Integer.parseInt(s[1]);
+            System.out.println(sum[end] - sum[start - 1]);
+        }
+    }
+}
---
+++ src/indi/xm/jy/acwing/questions/prefixsum/s0796/Main.java
@@ -0,0 +1,49 @@
+package indi.xm.jy.acwing.questions.prefixsum.s0796;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.acwing.questions.prefixsum.s0796
+ * @ClassName: Main
+ * @Author: albert.fang
+ * @Description: 子矩阵的和
+ * @Date: 2022/10/27 15:45
+ */
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] first = br.readLine().split(" ");
+        int n = Integer.parseInt(first[0]);
+        int m = Integer.parseInt(first[1]);
+        int q = Integer.parseInt(first[2]);
+        // 原素组
+        int[][] matrix = new int[n + 1][m + 1];
+        for (int i = 0; i < n; i++) {
+            String[] line = br.readLine().split(" ");
+            for (int j = 0; j < line.length; j++) {
+                matrix[i+1][j+1] = Integer.parseInt(line[j]);
+            }
+        }
+        // 二维前缀和数组
+        int[][] sum = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i][j];
+            }
+        }
+        for (int i = 0; i < q; i++) {
+            String[] axis = br.readLine().split(" ");
+            int x1 = Integer.parseInt(axis[0]);
+            int y1 = Integer.parseInt(axis[1]);
+            int x2 = Integer.parseInt(axis[2]);
+            int y2 = Integer.parseInt(axis[3]);
+            int ans = sum[x2][y2] - sum[x2][y1-1] - sum[x1-1][y2] + sum[x1-1][y1-1];
+            System.out.println(ans);
+        }
+    }
+}
