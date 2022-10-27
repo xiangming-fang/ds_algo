8deece42c7f6b1b146a60223a25eb18059b44ebf
方翔鸣 <48317891+xiangming-fang@users.noreply.github.com>
2022-10-27 18:52:00 +0800

    二维差分数组

---
+++ src/indi/xm/jy/leetcode/sn/SN0200/SN0085.java
@@ -0,0 +1,25 @@
+package indi.xm.jy.leetcode.sn.SN0200;
+
+/**
+ * @ProjectName: leetcode
+ * @Package: indi.xm.jy.leetcode.sn.SN0200
+ * @ClassName: SN0085
+ * @Author: albert.fang
+ * @Description: 最大矩阵
+ * @Date: 2022/10/27 16:47
+ */
+public class SN0085 {
+
+    public int maximalRectangle(char[][] matrix) {
+        int res = 0;
+        int[][] s = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int i = 1; i <= matrix.length; i++) {
+            for (int j = 1; j <= matrix[0].length; j++) {
+                s[i][j] = s[i-1][j] + s[i][j-1] - s[i-1][j-1] + matrix[i-1][j-1] - '0';
+            }
+        }
+
+        return res;
+    }
+
+}
