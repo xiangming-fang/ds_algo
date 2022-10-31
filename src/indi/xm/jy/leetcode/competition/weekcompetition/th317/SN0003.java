054ad3188840db38bd00e5e85ec3915f39a34712
方翔鸣 <48317891+xiangming-fang@users.noreply.github.com>
2022-10-31 11:00:32 +0800

    317-3

--- src/indi/xm/jy/leetcode/competition/weekcompetition/th317/SN0003.java
+++ src/indi/xm/jy/leetcode/competition/weekcompetition/th317/SN0003.java
@@ -11,12 +11,21 @@ import java.util.List;
  * @Package: indi.xm.jy.leetcode.competition.weekcompetition.th001
  * @ClassName: SN0001
  * @Author: albert.fang
- * @Description:
+ * @Description: 最小增量美丽整数
  * @Date: 2022/10/21 16:26
  */
 public class SN0003 {
 
-
+    // 思路：贪心
+    // 将n从左往右一个个累加，如果累加和大于target,记住当前位置，设当前位置为index。
+    // 则index往左的数字需要+1,index往右，并且包含index的位置全部置位0。
+    // 这样得到的一个数据就是增量最小的美丽整数
+    // eg：204932336 16
+    // 从左往右对204932336进行累加，第一次超出16的是迭代到第一个3,也就是index=5
+    // 将小于index部分加1，即2049+1 = 2050
+    // 将大于index的部分全部置为0,即00000
+    // 组合左边右边答案：2050 00000
+    // 最小增量为：205000000 - 204932336 = 67664.
     public long makeIntegerBeautiful(long n, int target) {
         long res = 0;
         if (getSumByBit(n) <= target) return 0;
@@ -42,7 +51,7 @@ public class SN0003 {
             }
             else numBit.add(tmp%10);
 
-            if (tmp > 10) tmp/=10;
+            if (tmp >= 10) tmp/=10;
             else break;
         }
         while (numBit.size() > 1 && numBit.get(numBit.size() - 1) == 0) numBit.remove(numBit.size() - 1);
@@ -83,6 +92,8 @@ public class SN0003 {
 //        System.out.println(makeIntegerBeautiful(999, 1));
 //        System.out.println(makeIntegerBeautiful(19, 1));
         System.out.println(makeIntegerBeautiful(204932336, 16));
+//        System.out.println(Integer.toBinaryString(30));
+//        System.out.println(205000000 - 204932336);
     }
 
 
