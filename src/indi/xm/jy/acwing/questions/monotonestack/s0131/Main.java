71648f2b6db108a133a6b15020630adc17b4b481
xiangming.fang <fang.albert@foxmail.com>
2022-11-17 00:27:37 +0800

    直方图中最大的矩形

--- src/indi/xm/jy/acwing/questions/monotonestack/s0131/Main.java
+++ src/indi/xm/jy/acwing/questions/monotonestack/s0131/Main.java
@@ -15,11 +15,10 @@ import java.io.InputStreamReader;
 public class Main {
 
     // 思路：
-    // 1、算出给定直方图的左一小（左边第一个小于当前位置的值，并且计算出小的位置）得到数组ans
-    // 2、ans每个位置有两个元素，一个是小的值 minV，一个是小的左边minI
-    // 3、如果是-1的话，那么就直接当前 (index + 1) * origin[index]
-    // 4、如果不是-1的话，那么就(index - minI + 1) * minV;
-    // 5、取上述结果最大值。
+    // 1、算出给定直方图的左一小（左边第一个小于当前位置的位置）得到数组lans，这里下标记为lindex;
+    // 2、算出给定直方图的右一小（右边第一个小于当前位置的位置）得到数组rans，这里下标记为rindex;
+    // 3、每个位置值(记为x)的最大面积是：x * (rindex - lindex - 1)
+    // 4、迭代每个位置，找到最大面积
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = 100010;
@@ -29,19 +28,32 @@ public class Main {
             int n = Integer.parseInt(s[0]);
             if ( n == 0) break;
             int[] origin = new int[n];
-            Pair<Integer,Integer>[] ans = new Pair[n];
+            int[] lans = new int[n];
+            int[] rans = new int[n];
             int tt = 0;
             for (int i = 1; i <= n; i++) {
                 origin[i-1] = Integer.parseInt(s[i]);
             }
             for (int i = 0; i < n; i++) {
-                while (tt > 0 && stk[tt] >= origin[i]) tt--;
+                while (tt > 0 && origin[stk[tt]] >= origin[i]) tt--;
+                if (tt==0) lans[i] = -1;
+                else lans[i] = stk[tt];
+                stk[++tt] = i;
             }
+            tt = 0;
+            for (int i = n - 1; i >= 0; i --){
+                while (tt > 0 && origin[stk[tt]] >= origin[i]) tt--;
+                if (tt == 0) rans[i] = n;
+                else rans[i] = stk[tt];
+                stk[++tt] = i;
+            }
+            long ans = 0;
+            for (int i = 0; i < n; i++) {
+                ans = Math.max(ans,(long)origin[i] * (rans[i] - lans[i] - 1));
+            }
+            System.out.println(ans);
         }
     }
-
-    static class Pair<K,V>{
-        public K k;
-        public V v;
-    }
 }
+
+// 9 1000000000 1000000000 1000000000 1000000000 1000000000 1000000000 999999999 1000000000 1000000000
