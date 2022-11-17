4ba394a4cf48357ce6c3420157c1823895f2ac35
方翔鸣 <48317891+xiangming-fang@users.noreply.github.com>
2022-11-17 14:01:55 +0800

    接雨水 - 单调栈

--- src/indi/xm/jy/acwing/questions/monotonestack/s1574/Main.java
+++ src/indi/xm/jy/acwing/questions/monotonestack/s1574/Main.java
@@ -14,7 +14,15 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
-
+    // 思路：以每个位置的元素为盛水容器的底部位置
+    // 1、求出每个位置的左1大和右1大的位置，分别用数组lans、rans存储，如果某个位置不存在左1大或者右1大，那么填充-1
+    // 2、定义origin数组为柱子的高度数组、当前位置为i，当前位置的为底的盛水左边界高度是 lh = origin[lans[i]]、rh = origin[rans[i]]
+    // 3、取lh、rh的小值，记为minH，那么以当前位置高度为底的可以盛水是：左右边界距离 * (minH - 当前位置高度) ==> (rh - lh - 1) * (minH - origin[i])
+    //    1 - 3步是主要思想
+    // 4、因为在同一个盛水区间里，可能存在高度相同，并且左右边界也相同的情况，这样就会重复计算盛水空间，所以我们需要这样一个标示
+    // 即 高度 ==> lh、rh 唯一，如果存在，则跳过当前迭代位置
+    // 举个例子说明第4步的必要性，如果只考虑1-3步骤，那么在以下 i = 4和i = 6的时候（i从0开始），会出现重复计算的情景，使得结果多了3个盛水区间。
+    // 0 1 0 2 1 0 1 3 2 1 2 1
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -43,14 +51,15 @@ public class Main {
         }
 
         int ans = 0;
+        // 确保以这个height为底的盛水区间不被重复计算
+        int[][] exist = new int[10010][2];
         for (int i = 0; i < n; i ++) {
             if (lans[i] == -1 || rans[i] == -1) continue;
-            System.out.println("装水下标： " + i);
+            if (exist[origin[i]][0] == lans[i] && exist[origin[i]][1] == rans[i]) continue;
+            exist[origin[i]][0] = lans[i];
+            exist[origin[i]][1] = rans[i];
             int minHeight = Math.min(origin[lans[i]], origin[rans[i]]);
-            for (int j = lans[i] + 1; j < rans[i]; j++) {
-                ans += minHeight - origin[j];
-            }
-            i = rans[i] - 1;
+            ans += (minHeight - origin[i]) * (rans[i] - lans[i] - 1);
         }
         System.out.println(ans + " ");
     }
@@ -61,3 +70,7 @@ public class Main {
 // 10
 // 0 10 4 6 5 0 9 8 10 2
 // 28
+
+// 12
+// 0 1 0 2 1 0 1 3 2 1 2 1
+// 6
