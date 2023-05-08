9052567bf51b179e3295b712293120bcb213d2e4
xiangming.fang <fang.albert@foxmail.com>
2023-05-08 16:41:56 +0800

    fix

---
+++ src/indi/xm/data_structure/hash/Main.java
@@ -0,0 +1,39 @@
+package indi.xm.data_structure.hash;
+
+
+
+import indi.xm.data_structure.map.LinkedListMap;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
+/**
+ * @Author: xiangming.fang
+ * @Date: 2023/3/29 17:03
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 链表？哈希？
+        LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>(16,0.5f,true);
+        map.put(2,324);
+        map.put(456,234);
+        map.put(789,234);
+        map.put(23,234);
+        map.put(20,324);
+        Integer res20 = map.get(789);
+        map.entrySet().forEach(v -> System.out.println(v.toString()));
+        System.out.println("*****************");
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+        set.add(2);
+        set.add(234);
+        set.add(23);
+        set.add(22);
+        set.add(20);
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
--- src/indi/xm/ds_algo/acwing/basic/basicalgo/QuickSort.java
+++ src/indi/xm/ds_algo/acwing/basic/basicalgo/QuickSort.java
@@ -1,5 +1,7 @@
 package indi.xm.ds_algo.acwing.basic.basicalgo;
 
+import java.util.Arrays;
+
 /**
  * @ProjectName: leetcode
  * @Package: indi.xm.jy.acwing.questions.algo.template
@@ -15,6 +17,7 @@ public class QuickSort {
     // 小于等于 分界点的放左边
     // 大于等于 分界点的放右边
     // 递归排序左边，递归排序右边
+    // todo 中间放的并不是等于基值的值，这个算法只是分了两边，而不是三块
     public void quickSort(int[] arr,int l,int r){
         if ( l >= r ) return;
         int i = l - 1,j = r + 1;
@@ -30,6 +33,11 @@ public class QuickSort {
         }
         quickSort(arr,l,j);
         quickSort(arr,j + 1,r);
+    }
+
+    public static void main(String[] args) {
+        QuickSort quickSort = new QuickSort();
+        quickSort.quickSort(new int[]{2,8,7,1,3,5,6,4},0,7);
     }
 
 }
